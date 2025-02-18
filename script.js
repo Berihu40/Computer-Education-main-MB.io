8905c0b1a44dbd39808bb91960c4c7cc6f76dd69
Merhawi15 <merhawiberihu15@gmail.com>
2025-02-18 14:43:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example of a simple JavaScript function
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to my website!");
+    // Additional interactivity can be added here
+  });
+  
